2239252c0c48e8285adee64e74bccf14219e1117
Aroze <admin@aroze.me>
2024-11-17 21:12:37 +0000

    Initial commit

---
+++ src/main/kotlin/me/aroze/spyglass/Spyglass.kt
@@ -0,0 +1,14 @@
+package me.aroze.spyglass
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Spyglass : JavaPlugin() {
+
+    override fun onEnable() {
+        // meow meow
+    }
+
+    override fun onDisable() {
+        // miaow ;3
+    }
+}
